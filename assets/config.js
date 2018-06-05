5e33c4cf2afbe9b30feeb2bba06fab22447c86f7
spensation <spensation@gmail.com>
2018-06-05 14:07:28 -0700

    restyled navbar and links

---
+++ assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "spencevans@gmail.com",
+        address: "",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "",
+            title: "Spencer Evans",
+            subtitle: "Full-Stack Developer",
+            type: 'vcard',
+            backgroundImage: "\/assets\/images\/dev\/tropics.jpg",
+            backgroundColor: "#646389",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: "assets\/images\/dev\/rothko.jpg"
+        }
+    ]
+};
